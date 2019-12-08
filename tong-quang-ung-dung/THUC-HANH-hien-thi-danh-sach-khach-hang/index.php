4c1924977c5706b6df4bc2eaae8c3207c6026aa0
hieudanghuu <hieudanghuu08091989@gmail.com>
2019-12-08 08:03:47 +0700

    danh sách khách hàng

---
+++ tong-quang-ung-dung/THUC-HANH-hien-thi-danh-sach-khach-hang/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+table {
+    border-collapse: collapse;
+    width:100%
+}
+th,td {
+    padding:8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+.thumbnail{
+    height : 60px;
+    width : 100px;
+    overflow: hidden;
+    
+}
+.thumbnail img{  
+    width : 100%
+}
+.thumbnail img {
+    height : 100% 
+}
+</style>
+<body>
+    <table border = "0">
+    <caption><h1>DANH SÁCH KHÁCH HÀNG</h1></caption>
+    <tr>
+    <th>STT</th>
+    <th>Tên</th>
+    <th>Ngày sinh</th>
+    <th>Địa Chỉ</th>
+    <th>Ảnh</th>
+    </tr>
+    
+    <?php
+    $customerlist = array(
+        "1" => array ("ten" => "Mai Văn Hoàng",
+                      "ngaysinh" => "1983-03-08",
+                      "diachi" => "Hà Nội",
+                      "anh" => "anh1.jpg"),
+        "2" => array ("ten" => "Nguyễn Văn Nam",
+                      "ngaysinh" => "1989-09-05",
+                      "diachi" => "Nam Định",
+                      "anh" => "anh2.jpg"),
+        "3" => array ("ten" => "Nguyễn Thị Thái Hòa",
+                      "ngaysinh" => "1992-09-12",
+                      "diachi" => "Huế",
+                      "anh" => "anh3.jpg"),
+        "4" => array ("ten" => "Trần Đăng Khoa",
+                      "ngaysinh" => "1991-03-18",
+                      "diachi" =>"Hà Tây",
+                      "anh" => "anh4.jpg"),
+        "5" => array ("ten" => "Nguyễn Đình Thi",
+                      "ngaysinh" => "1990-03-19",
+                      "diachi" => " HÀ Nội",
+                      "anh" => "anh5.jpg")                       
+        );
+        foreach($customerlist as $key =>$value){
+            echo "<tr>";
+            echo "<td>".$key."</td>";
+            echo "<td>".$value["ten"]."</td>";
+            echo "<td>".$value["ngaysinh"]."</td>";
+            echo "<td>".$value['diachi']."</td>";
+            echo "<td><div class='thumbnail'><image src = '".$value['anh']."'/></div></td>";
+            echo "</tr>";
+        }
+    ?>
+</table>
+</body>
+</html>
--- tong-quang-ung-dung/server_time/index.php
+++ tong-quang-ung-dung/server_time/index.php
@@ -1,19 +1,12 @@
 <html>
- <body>
- <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-   Name: <input type="text" name="fname">
-   <input type="submit">
- </form>
- <?php
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     /* collect value of input field */
-     $name = $_POST['fname'];
-     if (empty($name)) {
-         echo "Name is empty";
-     } else {
-         echo $name;
-     }
- }
-?> 
-</body>
+    <head>
+        <title>Current server time</title>
+    </head>
+    <body>
+        <h1> Current Server Time is: 
+        <?php 
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        echo date('Y-M-d h:m:s'); ?>
+        </h1>
+    </body>
 </html>
