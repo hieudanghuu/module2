892ec905dd598a0a0d4ea3f4eb7731baa7d20d1c
hieudanghuu <hieudanghuu08091989@gmail.com>
2019-12-14 12:12:08 +0700

    lớp Point2D và Point3D

---
+++ Inherritance-KE-THUA/BAI_TAP-lop-Point2D-va-lop-Point3D/Point2D.php
@@ -0,0 +1,42 @@
+<?php
+class Point2d
+{
+    private $x;
+    private $y;
+    public function __construct($x,$y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+    public function getX(){
+        return $this->x;
+    }
+    public function getY(){
+        return $this->y;
+    }
+    public function setX($x){
+        $this->x = $x;
+    }
+    public function setY($y){
+        $this->y = $y;
+    }
+    public function getXY(){
+        return "Array of {". $this->x.",".$this->y;
+    }
+    public function setXY($x,$y){
+        $this->x = $x;
+        $this->y = $y;
+    }
+    public function toString(){
+        return '"('.$this->x.",".$this->y;
+    }
+}
+include_once ("Point3D.php");
+$point2d = new Point2d(2,4);
+echo $point2d-> getXY() ."} <br>";
+echo $point2d-> toString().')" <br><br>';
+
+$point3d = new Point3d(3,5,7);
+echo $point3d-> getXYZ()."<br>";
+echo $point3d-> toString();
+?>
